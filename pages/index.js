3e2b35de353d692e3954b692c35182194bb64a8f
mszoom24 <mszoom24@gmail.com>
2025-07-31 15:52:26 +0800

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="p-6">
+      <Head>
+        <title>Bukti Potong Pajak</title>
+      </Head>
+      <h1 className="text-2xl font-bold">Aplikasi Bukti Potong Pajak</h1>
+      <p>Halo! Ini adalah proyek Next.js v12 + Tailwind CSS</p>
+    </div>
+  )
+}
